2deee06185b4d33ff7db1b3f7ca8befa825450b1
INTI-0246 <INTI-0246@INTI0246>
2016-06-08 14:31:16 +0200

    ajout classe client 2

---
+++ proxyBanqueV1/src/main/java/fr/proxybanque/model/Client.java
@@ -0,0 +1,118 @@
+package fr.proxybanque.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clients")
+public class Client {
+	
+	@Id
+	@Column(name = "id_client")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id_client;
+	private String nom;
+	private String prenom;
+	private String adresse;
+	private String codeP;
+	private String ville;
+	private String telephone;
+	
+	/**
+	 * Constructeurs
+	 */
+	public Client() {
+		super();
+	}
+
+	public Client(String nom, String prenom, String adresse, String codeP, String ville, String telephone) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.adresse = adresse;
+		this.codeP = codeP;
+		this.ville = ville;
+		this.telephone = telephone;
+	}
+
+	public Client(int id_client, String nom, String prenom, String adresse, String codeP, String ville,
+			String telephone) {
+		super();
+		this.id_client = id_client;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.adresse = adresse;
+		this.codeP = codeP;
+		this.ville = ville;
+		this.telephone = telephone;
+	}
+
+	/**
+	 * GETTERS / SETTERS
+	 */
+	
+	public int getId_client() {
+		return id_client;
+	}
+
+	public void setId_client(int id_client) {
+		this.id_client = id_client;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public String getCodeP() {
+		return codeP;
+	}
+
+	public void setCodeP(String codeP) {
+		this.codeP = codeP;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	
+	
+
+	
+	
+}
